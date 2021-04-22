b023e4eab9654ddc166a7de7a7910b8c74e91117
Bárbara Geovanna Lima Sol <barbarasol027@gmail.com>
2021-04-22 16:12:12 -0300

    Criando classe vacina

---
+++ controleVacinacao/src/controleVacinacao/classes/Vacina.java
@@ -0,0 +1,15 @@
+package controleVacinacao.classes;
+
+public class Vacina {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
