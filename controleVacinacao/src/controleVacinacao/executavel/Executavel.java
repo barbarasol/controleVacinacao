e7629c853e0cbace2574dea09007ba9aa99af2fe
Bárbara Geovanna Lima Sol <barbarasol027@gmail.com>
2021-04-22 17:13:25 -0300

    Criando menu

--- controleVacinacao/src/controleVacinacao/executavel/Executavel.java
+++ controleVacinacao/src/controleVacinacao/executavel/Executavel.java
@@ -1,10 +1,52 @@
 package controleVacinacao.executavel;
 
+import java.util.Scanner;
+
+//import javax.swing.JOptionPane;
+
+//import controleVacinacao.classes.Pessoa;
+
 public class Executavel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		do {
+			System.out.println("Escolha uma opção:");
+			System.out.println("1 - Realizar Cadastro");
+			System.out.println("2 - Agendar vacinação");
+			System.out.println("3 - Consultar dados");
+			System.out.println("4 - Sair");
+			int op = scan.nextInt();
+			switch(op) {
+				case 1:
+					//cadastrar();
+					break;
+				case 2:
+					
+					break;
+				case 3:
+					
+					break;
+				case 4:
+					//exit(1);
+			}
+			
+					
+			
+		}while(true);
+		
+		
+		
+		
+		//Pessoa p1 = new Pessoa();
+		
+		
+		//String nome = JOptionPane.showInputDialog("Qual o nome da pessoa ?");
+		
+		//p1.setNome(nome);
+		
+		//System.out.println(p1);
 	}
 
 }
