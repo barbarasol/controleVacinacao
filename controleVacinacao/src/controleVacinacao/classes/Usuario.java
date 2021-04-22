3fa2796b2ce92865135c8e911842da61446e053c
Bárbara Geovanna Lima Sol <barbarasol027@gmail.com>
2021-04-22 17:34:35 -0300

    Criando classe usuario

---
+++ controleVacinacao/src/controleVacinacao/classes/Usuario.java
@@ -0,0 +1,28 @@
+package controleVacinacao.classes;
+
+public class Usuario extends Pessoa {
+	private String endereco;
+	private int idade;
+	private String telefone;
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
